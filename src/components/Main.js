4ed1b1d9c3b5b53fe6d614c89c44e61e488ad0d3
Zach Rojas <zachrojas94@gmail.com>
2022-02-28 17:01:45 -0600

    readme

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,12 @@
-import React from 'react'
-import Navigation from './Navigation'
+import React from 'react';
+import Navigation from './Navigation';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Footer from './Footer';
 
-const Header = ( { currentPage, handlePageChange } ) => {
+const Main = ( { currentPage, handlePageChange } ) => {
     return (
         <div>
             <Navigation currentPage={currentPage} handlePageChange={handlePageChange} />
@@ -9,4 +14,4 @@ const Header = ( { currentPage, handlePageChange } ) => {
     )
 };
 
-export default Header
+export default Main;
